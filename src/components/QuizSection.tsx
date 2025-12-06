2a803de282738b4adcbcc4609ed65bde1ac71559
ViniciusMoraesTavares <viniciusmoraestavares2020@gmail.com>
2025-12-06 19:25:22 -0300

    07-12-25 v2

--- src/components/QuizSection.tsx
+++ src/components/QuizSection.tsx
@@ -32,7 +32,7 @@ const quizReducer = (state: QuizState, action: QuizAction): QuizState => {
 };
 
 const QuizSection: React.FC<QuizSectionProps> = ({ questions }) => {
-  const QUIZ_VERSION = '30-11-2025';
+  const QUIZ_VERSION = '07-12-2025';
   const quizKey = `quiz_${QUIZ_VERSION}`;
 
   const savedState = localStorage.getItem(quizKey);
