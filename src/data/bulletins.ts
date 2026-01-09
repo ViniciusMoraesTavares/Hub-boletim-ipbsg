26e7725f77d80488e5e81112bc0268c13b723249
ViniciusMoraesTavares <viniciusmoraestavares2020@gmail.com>
2026-01-09 15:12:18 -0300

    11/01/2026 v1

--- src/data/bulletins.ts
+++ src/data/bulletins.ts
@@ -1,37 +1,37 @@
 import { Bulletin } from '../types';
 
+import pdf687 from '/pdf/687 - APROXIME SEU FILHO DA IGREJA - Boletim virtual.pdf';
 import pdf685 from '/pdf/685 - AGRACIADOS PARA SERMOS CANAIS DE GRAÇA - Boletim virtual.pdf';
 import pdf684 from '/pdf/684 - O SENTIDO DA VIDA ESTÁ EM DEUS pt II  - Boletim virtual.pdf';
 import pdf683 from '/pdf/683 - O SENTIDO DA VIDA ESTÁ EM DEUS - Boletim virtual.pdf';
-import pdf682 from '/pdf/682 - A igreja deve ser testemunha aqui e também lá! - Boletim virtual.pdf';
 
 // Boletim atual disponível sem PDF: manter título, data e versão
 export const currentBulletin: Partial<Bulletin> & Pick<Bulletin, 'title' | 'date' | 'version'> = {
-  title: 'AGRACIADOS PARA SERMOS CANAIS DE GRAÇA',
-  date: '28/12/2025',
-  version: '685',
-  pdf: pdf685,
+  title: 'APROXIME SEU FILHO DA IGREJA',
+  date: '11/01/2026',
+  version: '687',
+  pdf: pdf687,
 };
 
 export const previousBulletin1: Bulletin = {
+  title: 'AGRACIADOS PARA SERMOS CANAIS DE GRAÇA',
+  date: '28/12/2025',
+  pdf: pdf685,
+  version: '685',
+};
+
+export const previousBulletin2: Bulletin = {
   title: 'O SENTIDO DA VIDA ESTÁ EM DEUS pt II',
   date: '21/12/2025',
   pdf: pdf684,
   version: '684',
 };
 
-export const previousBulletin2: Bulletin = {
+export const previousBulletin3: Bulletin = {
   title: 'O SENTIDO DA VIDA ESTÁ EM DEUS',
   date: '14/12/2025',
   pdf: pdf683,
   version: '683',
-};
-
-export const previousBulletin3: Bulletin = {
-  title: 'A igreja deve ser testemunha aqui e também lá!',
-  date: '07/12/2025',
-  pdf: pdf682,
-  version: '682',
 };
 
 export const previousBulletins: Bulletin[] = [
