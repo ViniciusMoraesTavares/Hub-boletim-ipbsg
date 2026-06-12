c9cdcbad76ca8d6da6ac60eac07b61edefd30510
Vinicius Moraes Tavares <130687376+ViniciusMoraesTavares@users.noreply.github.com>
2026-06-12 17:42:42 -0300

    Add new bulletin and update previous entries
    Added a new current bulletin and updated previous bulletins.

--- src/data/bulletins.ts
+++ src/data/bulletins.ts
@@ -1,37 +1,37 @@
 import { Bulletin } from '../types';
 
+import pdf709 from '/pdf/709 - O Valor de uma Escolha De Bitcoins a Porcos - Boletim virtual.pdf';
 import pdf708 from '/pdf/708 - Com Cristo no barco tudo vai muito bem - Boletim virtual.pdf';
 import pdf707 from '/pdf/707 - Quão bom é estarmos em união - Boletim virtual.pdf';
 import pdf706 from '/pdf/706 - O poder da Palavra e o Reino de Deus - Boletim virtual.pdf';
-import pdf704 from '/pdf/704 - O PECADO IMPERDOÁVEL - Boletim virtual.pdf';
 
 // Boletim atual (mais recente)
 export const currentBulletin: Bulletin = {
+  title: 'O Valor de uma Escolha: De Bitcoins a Porcos',
+  date: '14/06/2026',
+  version: '709',
+  pdf: pdf709,
+};
+
+export const previousBulletin1: Bulletin = {
   title: 'COM CRISTO NO BARCO TUDO VAI MUITO BEM',
   date: '07/06/2026',
   version: '708',
   pdf: pdf708,
 };
 
-export const previousBulletin1: Bulletin = {
+export const previousBulletin2: Bulletin = {
   title: 'QUÃO BOM É ESTARMOS EM UNIÃO',
   date: '31/05/2026',
   version: '707',
   pdf: pdf707,
 };
 
-export const previousBulletin2: Bulletin = {
+export const previousBulletin3: Bulletin = {
   title: 'O poder da Palavra e o Reino de Deus',
   date: '24/05/2026',
   version: '706',
   pdf: pdf706,
-};
-
-export const previousBulletin3: Bulletin = {
-  title: 'O PECADO IMPERDOÁVEL',
-  date: '10/05/2026',
-  version: '704',
-  pdf: pdf704,
 };
 
 export const previousBulletins: Bulletin[] = [
@@ -44,10 +44,10 @@ export const previousBulletins: Bulletin[] = [
 export const bulletins: Bulletin[] = [currentBulletin, ...previousBulletins];
 
 const __datesOk = (
-  currentBulletin.date === '07/06/2026' &&
-  previousBulletin1.date === '31/05/2026' &&
-  previousBulletin2.date === '24/05/2026' &&
-  previousBulletin3.date === '10/05/2026'
+  currentBulletin.date === '14/06/2026' &&
+  previousBulletin1.date === '07/06/2026' &&
+  previousBulletin2.date === '31/05/2026' &&
+  previousBulletin3.date === '24/05/2026'
 );
 
 if (!__datesOk) {
