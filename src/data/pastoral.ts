7167775f6d4d4763f6235bb960ca90813a5d6a56
Vinicius Moraes Tavares <130687376+ViniciusMoraesTavares@users.noreply.github.com>
2026-06-12 17:50:23 -0300

    Update pastoral title and modify paragraphs

--- src/data/pastoral.ts
+++ src/data/pastoral.ts
@@ -1,13 +1,11 @@
 export const pastoral = {
-  title: 'COM CRISTO NO BARCO TUDO VAI MUITO BEM',
+  title: 'O VALOR DE UMA ESCOLHA: DE BITCOINS A PORCOS',
   author: 'Rev. Júlio Alexandre',
   paragraphs: [
-    'As tempestades da vida são passíveis a todos nós e podem vir como um problema financeiro, um relacionamento destruído, a saúde comprometida, a perda dos filhos, uma tragédia, o abalo no trabalho, uma perseguição injusta e de muitas outras formas. Não existe uma regra, assim como também não existe um meio de ficar imune às tribulações.',
+    'Em 22 de maio de 2010, Laszlo Hanyecz abriu mão de 10.000 bitcoins por duas pizzas que somadas custavam apenas 55 reais. Hoje você compra duas pizzas por menos de 100 reais, mas 10.000 bitcoins é impossível de ser ajuntado, pois hoje valem três bilhões, duzentos e cinquenta e dois milhões, setecentos e oito mil, quinhentos e sessenta reais. Se aquele rapaz soubesse o valor real de um bitcoin seria hoje um bilionário, mas contentou-se com duas pizzas que não duraram uma noite.',
 
-    'Quando olhamos para este texto, vemos que a tribulação pode vir a qualquer momento, sem aviso prévio, sem um alerta. A tribulação não nos sobrevém apenas como consequência de nossos erros, mas pode vir até mesmo quando obedecemos ao chamado de Jesus e atendemos aos Seus pedidos. A tribulação pode se agravar a tal ponto de não vermos saída e alcançar uma dimensão da qual não temos controle algum.',
+    'No texto que acabamos de ler, algo semelhante aconteceu. Jesus chegou até a terra dos gerasenos e lá libertou um homem possesso, cujos espíritos imundos saindo dele entraram em porcos que se atiraram para dentro do mar. Os donos dos porcos, vendo o milagre realizado por Jesus e o prejuízo que tiveram com a perda dos porcos, foram até a cidade espantados e contaram aos cidadãos o que aconteceu.',
 
-    'A questão mais importante não é estar imune, mas estar com Jesus em todos os momentos. Há um cântico antigo que fala sobre isso e diz: "Com Cristo no barco tudo vai muito bem e passa o temporal". Ele é quem pode trazer bonança e sossego; é aquele que controla todas as coisas, de modo que o vento e o mar Lhe obedecem.',
-
-    'Somos desafiados a crer em Cristo e confiar no Seu poder, descansando em Sua soberania, lançando sobre Ele toda a nossa ansiedade, tendo paz em Cristo e sabendo que todas as coisas cooperam para o bem daqueles que amam a Deus.'
+    'Quando eles foram ver o que tinha acontecido e encontraram o endemoninhado são, viram o que sucedeu com os porcos e suplicaram a Jesus que fosse embora daquele lugar, rejeitando um tesouro eterno por conta de bens temporários. Infelizmente, ainda hoje muitos trocam bênçãos eternas por prazeres temporários que nada valem diante da oferta trazida por Jesus.'
   ],
 };
