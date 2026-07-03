09d2e02b3dd60ef0cf98f94ee2b5491539d4214c
Vinicius Moraes Tavares <130687376+ViniciusMoraesTavares@users.noreply.github.com>
2026-07-03 16:04:59 -0300

    Update bulletins with new current and previous versions
    Added a new current bulletin and updated previous bulletins. Removed the previous bulletin for version 708.

--- src/data/bulletins.ts
+++ src/data/bulletins.ts
@@ -1,37 +1,37 @@
 import { Bulletin } from '../types';
 
+import pdf712 from '/pdf/712 - As faces da incredulidade - Boletim virtual.pdf';
 import pdf711 from '/pdf/711 - A unção com óleo Símbolo, não amuleto - Boletim virtual.pdf';
 import pdf710 from '/pdf/710 - A Fé que Sustenta a Oração - Boletim virtual.pdf';
 import pdf709 from '/pdf/709 - O Valor de uma Escolha De Bitcoins a Porcos - Boletim virtual.pdf';
-import pdf708 from '/pdf/708 - Com Cristo no barco tudo vai muito bem - Boletim virtual.pdf';
 
 // Boletim atual (mais recente)
 export const currentBulletin: Bulletin = {
+  title: 'AS FACES DA INCREDULIDADE',
+  date: '05/07/2026',
+  version: '712',
+  pdf: pdf712,
+};
+
+export const previousBulletin1: Bulletin = {
   title: 'A unção com óleo: Símbolo, não amuleto',
   date: '28/06/2026',
   version: '711',
   pdf: pdf711,
 };
 
-export const previousBulletin1: Bulletin = {
+export const previousBulletin2: Bulletin = {
   title: 'A Fé que Sustenta a Oração',
   date: '21/06/2026',
   version: '710',
   pdf: pdf710,
 };
 
-export const previousBulletin2: Bulletin = {
+export const previousBulletin3: Bulletin = {
   title: 'O Valor de uma Escolha: De Bitcoins a Porcos',
   date: '14/06/2026',
   version: '709',
   pdf: pdf709,
-};
-
-export const previousBulletin3: Bulletin = {
-  title: 'COM CRISTO NO BARCO TUDO VAI MUITO BEM',
-  date: '07/06/2026',
-  version: '708',
-  pdf: pdf708,
 };
 
 export const previousBulletins: Bulletin[] = [
@@ -44,10 +44,10 @@ export const previousBulletins: Bulletin[] = [
 export const bulletins: Bulletin[] = [currentBulletin, ...previousBulletins];
 
 const __datesOk = (
-  currentBulletin.date === '28/06/2026' &&
-  previousBulletin1.date === '21/06/2026' &&
-  previousBulletin2.date === '14/06/2026' &&
-  previousBulletin3.date === '07/06/2026'
+  currentBulletin.date === '05/07/2026' &&
+  previousBulletin1.date === '28/06/2026' &&
+  previousBulletin2.date === '21/06/2026' &&
+  previousBulletin3.date === '14/06/2026'
 );
 
 if (!__datesOk) {
