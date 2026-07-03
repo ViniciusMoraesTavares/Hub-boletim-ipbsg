3c85f7145e44126264b84f15a5e6abf27223ab00
Vinicius Moraes Tavares <130687376+ViniciusMoraesTavares@users.noreply.github.com>
2026-07-03 16:05:22 -0300

    Update quiz version to '05-07-2026'

--- src/components/QuizSection.tsx
+++ src/components/QuizSection.tsx
@@ -32,7 +32,7 @@ const quizReducer = (state: QuizState, action: QuizAction): QuizState => {
 };
 
 const QuizSection: React.FC<QuizSectionProps> = ({ questions }) => {
-  const QUIZ_VERSION = '28-06-2026';
+  const QUIZ_VERSION = '05-07-2026';
   const quizKey = `quiz_${QUIZ_VERSION}`;
 
   const savedState = localStorage.getItem(quizKey);
