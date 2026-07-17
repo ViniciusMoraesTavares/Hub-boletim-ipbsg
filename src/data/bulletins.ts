616763577d219a56b5e98045d958e28698fad962
Vinicius Moraes Tavares <130687376+ViniciusMoraesTavares@users.noreply.github.com>
2026-07-17 16:01:28 -0300

    Update bulletins with new current and previous entries

--- src/data/bulletins.ts
+++ src/data/bulletins.ts
@@ -1,37 +1,37 @@
 import { Bulletin } from '../types';
 
+import pdf714 from '/pdf/714 - Distorção do corbã - Boletim virtual.pdf';
 import pdf713 from '/pdf/713 - O PASTOR DA NOSSA ALMA - Boletim virtual.pdf';
 import pdf712 from '/pdf/712 - As faces da incredulidade - Boletim virtual.pdf';
 import pdf711 from '/pdf/711 - A unção com óleo Símbolo, não amuleto - Boletim virtual.pdf';
-import pdf710 from '/pdf/710 - A Fé que Sustenta a Oração - Boletim virtual.pdf';
 
 // Boletim atual (mais recente)
 export const currentBulletin: Bulletin = {
+  title: 'DISTORÇÃO DO CORBÃ',
+  date: '19/07/2026',
+  version: '714',
+  pdf: pdf714,
+};
+
+export const previousBulletin1: Bulletin = {
   title: 'O PASTOR DA NOSSA ALMA',
   date: '12/07/2026',
   version: '713',
   pdf: pdf713,
 };
 
-export const previousBulletin1: Bulletin = {
+export const previousBulletin2: Bulletin = {
   title: 'AS FACES DA INCREDULIDADE',
   date: '05/07/2026',
   version: '712',
   pdf: pdf712,
 };
 
-export const previousBulletin2: Bulletin = {
+export const previousBulletin3: Bulletin = {
   title: 'A unção com óleo: Símbolo, não amuleto',
   date: '28/06/2026',
   version: '711',
   pdf: pdf711,
-};
-
-export const previousBulletin3: Bulletin = {
-  title: 'A Fé que Sustenta a Oração',
-  date: '21/06/2026',
-  version: '710',
-  pdf: pdf710,
 };
 
 export const previousBulletins: Bulletin[] = [
@@ -44,10 +44,10 @@ export const previousBulletins: Bulletin[] = [
 export const bulletins: Bulletin[] = [currentBulletin, ...previousBulletins];
 
 const __datesOk = (
-  currentBulletin.date === '12/07/2026' &&
-  previousBulletin1.date === '05/07/2026' &&
-  previousBulletin2.date === '28/06/2026' &&
-  previousBulletin3.date === '21/06/2026'
+  currentBulletin.date === '19/07/2026' &&
+  previousBulletin1.date === '12/07/2026' &&
+  previousBulletin2.date === '05/07/2026' &&
+  previousBulletin3.date === '28/06/2026'
 );
 
 if (!__datesOk) {
